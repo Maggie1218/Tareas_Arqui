41e0f19d1a306c4986c8bb7f01bffe8a73412ef5
Margarita Jauregui Franco <margarita.jaureguif@gmail.com>
2018-02-15 21:16:32 -0600

    Utilizando stack y recursividad

--- Fibonacci.asm
+++ Fibonacci.asm
@@ -1,22 +1,42 @@
 .data
-n: .word 6
+n: .word 3
 .text
    lw $a0, n
+   jal fibonacci
+   j exit
+
 
 fibonacci:
-    slti $t0, $a0,2
-    beq $t0, 1, base
-
+    addi $sp, $sp, -12
+    sw $a0, 0($sp)
+    sw $v0, 4($sp)
+    sw $ra, 8($sp)    
+    slti $t0, $a0, 2
+    bne $t0, 1, general
+    jal base
+    beq $a0, 0, exitFibonacci
+    
 general:
     addi $a0, $a0, -1
     jal fibonacci
-    add $v1, $v1, $v0
+    add $t1, $v0, $zero
     addi $a0, $a0, -1
     jal fibonacci
-    add $v1, $v1, $v0
+    add $v0, $v0, $t1
+
+exitFibonacci:
+   
+    lw $a0, 0($sp)
+    lw $v0, 4($sp)  
+    lw $ra, 8($sp)
+    addi $sp, $sp, 12 
+    jr $ra      
+     
     
-    
+
 base:
     add $v0, $zero, $a0
+    jr $ra
+	
 
 exit:
